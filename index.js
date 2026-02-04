e7440adb14a718af4f2682dd095dd81d981ff5e6
RaghavDeo123 <r.deo.1370@westcliff.edu>
2026-02-04 10:32:44 +0545

    My First JavaScript

---
+++ index.js
@@ -0,0 +1,7 @@
+let person = {
+    first_name: "Raghav",
+    last_name: "Deo",
+    age: "20",
+    website: "Raghav@Deo.com"
+}
+console.log(`full name is ${person.first_name} ${person.last_name} `);
